4a537a4f98e0f4545a7809de8c3141fbb358e73b
AbdulrehmanAfzal22 <abdulrehmanafzal60@gmail.com>
2025-04-14 19:20:00 +0500

    javascript promgram+ changes in websitew

---
+++ practice.js
@@ -0,0 +1,16 @@
+let a = "Abdulrehman"
+let b = "Afzal"
+let c = " playing games"
+let d = "Physic"
+let e = a + b;
+
+console.log("My name is " + e, "I love" + c, "I'm quite good in " + d)
+
+let f = "Reading books"
+let g = "English"
+
+console.log("My name is " + e, "I love " + f, "I'm quite good in " + g)
+
+console.log("I'm quite good in "  + g , "I love " + f, "My name is " + e)
+
+              
